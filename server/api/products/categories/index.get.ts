9908f9014e2465caa0861430637dda756d6ed596
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2022-08-23 13:46:42 +0200

    Add route to get categories list

---
+++ server/api/products/categories/index.get.ts
@@ -0,0 +1,17 @@
+import { DbConnection } from '../../../lib/db'
+const db = DbConnection.getInstance().getConnection()
+
+/**
+ * route : /api/products/categories
+ * method : GET
+ * Return all the products categories
+ */
+export default defineEventHandler((event) => {
+  /*
+  Here to get the list of categories we first create an array containing all category fields from the products using the map() function :
+    array = products.map((product) => product.category)
+  Then we create a new array containing only the uniques values from the category field using a intermediary set that we spread in the new array
+    categories = [... new Set(array)]
+  */
+  return { categories: [...new Set(db.data?.products.map((product) => product.category))] || null }
+})
